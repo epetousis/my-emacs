b24256be6d921e3c17335cada44395738aa081c4
Evan Petousis <evan@petousis.net>
2026-02-23 11:36:59 +1100

    evan-config: fix deprecated hunspell reference

--- pkgs/by-name/e/evan-config/default.nix
+++ pkgs/by-name/e/evan-config/default.nix
@@ -16,8 +16,8 @@
   evil,
   fireplace,
   htmlize,
+  hunspell,
   hunspellDicts,
-  hunspellWithDicts,
   magit,
   markdown-mode,
   nix-mode,
@@ -31,7 +31,7 @@ trivialBuild {
   pname = "evan-config";
   version = "1970-01-01";
   src = replaceVars ./default.el {
-    hunspell = hunspellWithDicts [ hunspellDicts.en_AU-large ];
+    hunspell = hunspell.withDicts [ hunspellDicts.en_AU-large ];
   };
   packageRequires = [
     breadcrumb
